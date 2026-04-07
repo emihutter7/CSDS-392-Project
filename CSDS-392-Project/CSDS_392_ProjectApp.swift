284d76713d87aa686a8b50ac14d436a68b1062a3
emihutter7 <160561262+emihutter7@users.noreply.github.com>
2026-04-07 09:58:33 -0400

    Initial Commit

---
+++ CSDS-392-Project/CSDS_392_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CSDS_392_ProjectApp.swift
+//  CSDS-392-Project
+//
+//  Created by Emi Hutter-DeMarco on 4/7/26.
+//
+
+import SwiftUI
+
+@main
+struct CSDS_392_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
