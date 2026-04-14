a5a87635f841b4f1dc2a1c9f16160a0bb737bcdd
emihutter7 <160561262+emihutter7@users.noreply.github.com>
2026-04-14 10:07:39 -0400

    More backend files

---
+++ CSDS-392-Project/Budget.swift
@@ -0,0 +1,8 @@
+//
+//  Budget.swift
+//  CSDS-392-Project
+//
+//  Created by Emi Hutter-DeMarco on 4/14/26.
+//
+
+import Foundation
---
+++ CSDS-392-Project/Expense.swift
@@ -0,0 +1,8 @@
+//
+//  Expense.swift
+//  CSDS-392-Project
+//
+//  Created by Emi Hutter-DeMarco on 4/14/26.
+//
+
+import Foundation
