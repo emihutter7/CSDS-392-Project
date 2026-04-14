24d98c2778e245c3f7653d77ba03480555e82748
Alexandra Twitty <atwitty24@gmail.com>
2026-04-14 11:18:17 -0400

    Created toolbar in ContentView

--- CSDS-392-Project/ContentView.swift
+++ CSDS-392-Project/ContentView.swift
@@ -8,12 +8,57 @@
 import SwiftUI
 
 struct ContentView: View {
+    @Environment(AuthModel.self) private var authModel
+    @State private var showSignOut = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            Text("Welcome!")
+            Spacer()
+            
+            
+            }
+        .navigationDestination(isPresented: $showSignOut) {
+                        SignOutView()
+                    }
+        .toolbar {
+            ToolbarItem(placement: .bottomBar) {
+                Button (action: {}){
+                    Image(systemName: "house.fill")
+                        .resizable()
+                        .scaledToFit()
+                }
+            }
+            ToolbarItem(placement: .bottomBar) {
+                Button (action: {}){
+                    Image("ExpensesIcon")
+                        .resizable()
+                        .scaledToFit()
+                }
+            }
+            ToolbarItem(placement: .bottomBar) {
+                Button (action: {}){
+                    Image(systemName: "square.and.pencil")
+                        .resizable()
+                        .scaledToFit()
+                }
+            }
+            ToolbarItem(placement: .bottomBar) {
+                Button (action: {}){
+                    Image("ReportsIcon")
+                        .resizable()
+                        .scaledToFit()
+                }
+            }
+            ToolbarItem(placement: .bottomBar) {
+                Button(action: {
+                    showSignOut = true
+                }) {
+                    Image(systemName: "gearshape.fill")
+                        .resizable()
+                        .scaledToFit()
+                }
+            }
         }
         .padding()
     }
@@ -21,4 +66,5 @@ struct ContentView: View {
 
 #Preview {
     ContentView()
+        .environment(AuthModel())
 }
